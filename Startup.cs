dc2df143f2c052841ff41b9c2f453ca2ed5eed73
Kevin Humareda <humaredadavila5c@gmail.com>
2019-06-03 18:27:12 -0500

    StartUp

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,8 @@ namespace HomePet
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +26,15 @@ namespace HomePet
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
